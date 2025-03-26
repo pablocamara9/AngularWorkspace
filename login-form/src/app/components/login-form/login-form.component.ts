9e770cb7c37a7b5712e134ad0fcaa18998a6aae5
Pablo Camara <camara.gapab23@triana.salesianos.edu>
2025-03-26 14:34:17 +0100

    Minor changes

--- login-form/src/app/components/login-form/login-form.component.ts
+++ login-form/src/app/components/login-form/login-form.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.css'
 })
 export class LoginFormComponent {
+
+  email = ''
+  password = ''
+
+  processForm() {
+    console.log('Email: ' + this.email);
+    console.log('Contraseña ' + this.password);
+    
+  }
 
 }
