d6788724eddba03863f339d59c57d112fcfc3401
Pablo Camara <camara.gapab23@triana.salesianos.edu>
2025-03-27 14:25:52 +0100

    Agregado proyecto de monitorización de dispositivos.

---
+++ registro-dispositivos/src/app/components/registro-dispositivos/registro-dispositivos.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registro-dispositivos',
+  standalone: false,
+  templateUrl: './registro-dispositivos.component.html',
+  styleUrl: './registro-dispositivos.component.css'
+})
+export class RegistroDispositivosComponent {
+
+  title = 'Registro de Dispositivos AS Control'
+
+  resetFiltros() {
+    
+  }
+
+}
